0be95fa654189b460e875314884555761ff29cfc
chris <chris@soulware.kr>
2023-07-05 15:40:15 +0900

    = 연산 구현

--- src/model/Calculator.ts
+++ src/model/Calculator.ts
@@ -1,5 +1,4 @@
 import { Stack } from 'src/model/Stack';
-
 type Operator = '+' | '-' | '*' | '/';
 type Tool = 'AC' | '+/-' | '%' | '.' | '=';
 
@@ -7,79 +6,130 @@ export class Calculator {
   private numStack: Stack<number>;
   private opStack: Stack<Operator>;
   private opPriority: { [op in Operator]: number };
-  private _curNumInput: Nullable<string>;
+  private curNumInput: Nullable<string>;
+  private errorMessage: Nullable<string>;
+  private lastOperator: Nullable<Operator>;
+  private lastNumber: Nullable<number>;
 
   constructor() {
     this.numStack = new Stack<number>();
     this.opStack = new Stack<Operator>();
     this.opPriority = { '*': 2, '/': 2, '+': 1, '-': 1 };
-    this._curNumInput = null;
-  }
-
-  get curNumInput() {
-    return this._curNumInput;
-  }
-
-  set curNumInput(val) {
-    this._curNumInput = val;
+    this.curNumInput = null;
+    this.errorMessage = null;
+    this.lastOperator = null;
+    this.lastNumber = null;
   }
 
   public display() {
-    if (this.curNumInput !== null) {
-      return Number(this.curNumInput);
-    } else if (this.numTop()) {
-      return this.numTop();
-    } else {
-      return 0;
-    }
+    const result = (() => {
+      if (this.errorMessage) {
+        return this.errorMessage;
+      } else if (this.curNumInput !== null) {
+        return Number(this.curNumInput);
+      } else if (this.numTop() !== undefined) {
+        return this.numTop();
+      } else {
+        return 0;
+      }
+    })();
+
+    return result;
   }
 
-  public numPush(val: number) {
+  private numPush(val: number) {
     this.numStack.push(val);
   }
 
-  public numTop() {
+  private numTop() {
     return this.numStack.top();
   }
 
-  public numPop() {
+  private numPop() {
     return this.numStack.pop();
   }
 
-  public numLength() {
+  private numLength() {
     return this.numStack.length();
   }
 
-  public isNumEmpty() {
-    return this.numStack.isEmpty();
-  }
-
-  public opPush(val: Operator) {
+  private opPush(val: Operator) {
+    this.lastOperator = val;
     this.opStack.push(val);
   }
 
-  public opTop() {
+  private opTop() {
     return this.opStack.top();
   }
 
-  public opPop() {
+  private opPop() {
     return this.opStack.pop();
   }
 
-  public opLength() {
+  private opLength() {
     return this.opStack.length();
   }
 
-  public isOpEmpty() {
-    return this.opStack.isEmpty();
-  }
-
-  public clear() {
+  private clear() {
     this.numStack.clear();
     this.opStack.clear();
+    this.curNumInput = null;
+    this.errorMessage = null;
   }
 
-  public calculate(op1: number, op2: number, operator: Operator) {
+  private consume() {
+    const op1 = (() => {
+      if (this.numTop() !== undefined) {
+        return this.numPop();
+      } else {
+        if (this.curNumInput !== null) {
+          return Number(this.curNumInput);
+        } else {
+          return undefined;
+        }
+      }
+    })();
+
+    const op2 = (() => {
+      if (this.curNumInput === null) {
+        if (this.numTop() !== undefined) {
+          return this.numPop();
+        } else {
+          if (this.lastNumber !== null) {
+            return this.lastNumber;
+          } else {
+            return undefined;
+          }
+        }
+      } else {
+        if (this.curNumInput !== null) {
+          return Number(this.curNumInput);
+        } else {
+          return undefined;
+        }
+      }
+    })();
+    const op = (() => {
+      if (this.opTop() !== undefined) {
+        return this.opPop();
+      } else {
+        if (this.lastOperator !== null) {
+          return this.lastOperator;
+        } else {
+          return undefined;
+        }
+      }
+    })();
+
+    if (op1 === undefined || op2 === undefined || op === undefined) {
+      throw new Error('op1 op2 op type error');
+    }
+
+    this.numPush(this.calculate(op1, op2, op));
+    this.curNumInput = null;
+  }
+
+  private calculate(op1: number, op2: number, operator: Operator) {
     console.log('calculate', op1, op2, operator);
     switch (operator) {
       case '+':
@@ -89,111 +139,94 @@ export class Calculator {
       case '*':
         return op1 * op2;
       case '/':
+        if (op2 === 0) {
+          throw new Error('숫자 아님');
+        }
         return op1 / op2;
     }
   }
 
   public input(val: number | Operator | Tool) {
-    switch (val) {
-      case 0:
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case '.':
-        if (this.curNumInput) {
-          this.curNumInput += String(val);
-        } else {
-          this.curNumInput = String(val);
-        }
-        break;
-      case '+':
-      case '-':
-      case '*':
-      case '/':
-        const nextNumLength =
-          this.numLength() + (this.curNumInput !== null ? 1 : 0);
-        const nextOpLength = this.opLength() + 1;
-        if (nextNumLength === 0) {
-          console.log('nextNumLength === 0');
+    try {
+      switch (val) {
+        case 'AC':
+          this.clear();
           break;
-        }
-        if (nextNumLength < nextOpLength) {
-          console.log('nextNumLength < nextOpLength');
-          this.opPop();
-        }
-
-        let opTop = this.opTop();
-        if (opTop === undefined) {
-          // 1. 들어있던 연산자가 없는 경우
-          if (this.curNumInput === null) {
-            // 1.1 버퍼에 숫자가 없는 경우
-            console.log('1.1');
-            this.opPush(val);
+        case '=':
+          if (this.curNumInput !== null) {
+            this.lastNumber = Number(this.curNumInput);
+          }
+          console.log('lastNumber', this.lastNumber);
+          if (this.numLength() === 0) {
+            break;
           } else {
-            // 1.2 버퍼에 숫자가 있는 경우
-            console.log('1.2');
-            this.numPush(Number(this.curNumInput));
-            this.curNumInput = null;
-            this.opPush(val);
+            while (true) {
+              this.consume();
+              if (this.numLength() <= 1) {
+                break;
+              }
+            }
+          }
+
+          break;
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case '.':
+          if (this.curNumInput) {
+            this.curNumInput += String(val);
+          } else {
+            this.curNumInput = String(val);
           }
           break;
-        } else {
-          // 2. 들어있던 연산자가 있는 경우
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+          this.lastNumber = Number(this.curNumInput);
+          const nextNumLength =
+            this.numLength() + (this.curNumInput !== null ? 1 : 0);
+          const nextOpLength = this.opLength() + 1;
+          if (nextNumLength === 0) {
+            break;
+          }
+          if (nextNumLength < nextOpLength) {
+            this.opPop();
+          }
+
           while (true) {
-            opTop = this.opTop();
-            if (opTop === undefined) {
-              this.opPush(val);
-              break;
-            }
-            if (this.opPriority[opTop] < this.opPriority[val]) {
-              // 2.2 들어있던 연산자의 우선순위가 낮은 경우
-              if (this.curNumInput) {
-                // 2.2.1 버퍼에 숫자가 있는 경우
-                console.log('2.2.1');
+            const opTop = this.opTop();
+            if (
+              opTop === undefined ||
+              this.opPriority[opTop] < this.opPriority[val]
+            ) {
+              if (this.curNumInput !== null) {
                 this.numPush(Number(this.curNumInput));
                 this.curNumInput = null;
-              } else {
-                // 2.2.2 버퍼에 숫자가 없는 경우
-                console.log('2.2.2');
               }
               this.opPush(val);
               break;
             }
 
-            // 2.1 들어있던 연산자의 우선순위가 높거나 같은 경우
-            if (this.curNumInput === null) {
-              console.log('2.1.1');
-              // 2.1.1 버퍼에 숫자가 없는 경우
-              const op1 = this.numPop();
-              const op2 = this.numPop();
-              this.curNumInput = null;
-              const op = this.opPop();
-              if (op1 === undefined || op2 === undefined || op === undefined) {
-                throw new Error('');
-              }
-              this.numPush(this.calculate(op2, op1, op));
-            } else {
-              console.log('2.1.2');
-              // 2.1.2 버퍼에 숫자가 있는 경우
-              const op1 = this.numPop();
-              const op2 = Number(this.curNumInput);
-              this.curNumInput = null;
-              const op = this.opPop();
-              if (op1 === undefined || op2 === null || op === undefined) {
-                throw new Error('');
-              }
-              this.numPush(this.calculate(op1, op2, op));
-            }
+            this.consume();
           }
-        }
-
-        break;
+          break;
+      }
+    } catch (e: any) {
+      console.log('catch', e);
+      if (e.message) {
+        this.errorMessage = e.message.toString();
+      } else {
+        this.errorMessage = e.toString();
+      }
+      console.log('errorMessage', this.errorMessage);
     }
     console.log(this.numStack.log());
     console.log(this.opStack.log());
